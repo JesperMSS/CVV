5549e30f8b5a0df3178b60bc5d8176ccf2f30801
JesperMSS <62060621+JesperMSS@users.noreply.github.com>
2022-01-04 20:24:30 +0100

    Add project files.

---
+++ Migrations/202201041238039_project add.cs
@@ -0,0 +1,56 @@
+namespace CVSITEHT2021.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class projectadd : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Projects",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Title = c.String(nullable: false),
+                        Description = c.String(nullable: false),
+                        CreatedBy = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.ProjectCVs",
+                c => new
+                    {
+                        Project_ID = c.Int(nullable: false),
+                        CV_id = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.Project_ID, t.CV_id })
+                .ForeignKey("dbo.Projects", t => t.Project_ID, cascadeDelete: true)
+                .ForeignKey("dbo.CVs", t => t.CV_id, cascadeDelete: true)
+                .Index(t => t.Project_ID)
+                .Index(t => t.CV_id);
+            
+            AddColumn("dbo.CVs", "Name", c => c.String(nullable: false));
+            AddColumn("dbo.CVs", "PhoneNumber", c => c.String());
+            AddColumn("dbo.CVs", "Mail", c => c.String(nullable: false));
+            AddColumn("dbo.CVs", "Competences", c => c.String());
+            DropColumn("dbo.CVs", "competances");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.CVs", "competances", c => c.String());
+            DropForeignKey("dbo.ProjectCVs", "CV_id", "dbo.CVs");
+            DropForeignKey("dbo.ProjectCVs", "Project_ID", "dbo.Projects");
+            DropIndex("dbo.ProjectCVs", new[] { "CV_id" });
+            DropIndex("dbo.ProjectCVs", new[] { "Project_ID" });
+            DropColumn("dbo.CVs", "Competences");
+            DropColumn("dbo.CVs", "Mail");
+            DropColumn("dbo.CVs", "PhoneNumber");
+            DropColumn("dbo.CVs", "Name");
+            DropTable("dbo.ProjectCVs");
+            DropTable("dbo.Projects");
+        }
+    }
+}
---
+++ Models/Project.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations;
+
+namespace CVSITEHT2021.Models
+{
+    public class Project
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string CreatedBy { get; set; }
+
+        public virtual ICollection<CV> CVs { get; set; }
+
+
+    }
+}
