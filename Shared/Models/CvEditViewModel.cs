5846fb369402594706a58d714ebdf5496ed31a67
JesperMSS <62060621+JesperMSS@users.noreply.github.com>
2022-01-05 20:43:33 +0100

    Build

--- Shared/Models/CvEditViewModel.cs
+++ Shared/Models/CvEditViewModel.cs
@@ -1,11 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Web;
 
-namespace CVSITEHT2021.Shared.Models
+namespace Shared
 {
-    public class Class1
+    public class CvEditViewModel
     {
+        public int Id { get; set; }
+
+        [DisplayName("Name")]
+        public string Name { get; set; }
+
+        [DisplayName("Telefonnummer")]
+        public string PhoneNumber { get; set; }
+
+        [DisplayName("E-mail")]
+        public string Mail { get; set; }
+
+        [DisplayName("Utbildning")]
+        public string Education { get; set; }
+
+        [DisplayName("Arbetsplats")]
+        public string Workplace { get; set; }
+
+        [DisplayName("Kompetenser")]
+        public string Competences { get; set; }
+
+        [DisplayName("Bild")]
+        public HttpPostedFileBase Image { get; set; }
+        public string ExistingImagePath { get; set; }
+        public bool IsCreateCvView { get; set; }
+
+
+    }
+
+    public class CvDeleteModel
+    {
+        public int Id { get; set; }
     }
 }
